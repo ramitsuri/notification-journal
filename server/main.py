e420d3e74b7ff48dac01514f4b61908c7f395b21
Ramit Suri <ramitsuri007@gmail.com>
2023-10-18 15:16:14 -0400

    Reformat python file

--- server/main.py
+++ server/main.py
@@ -1,17 +1,15 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
 import logging
 import json
 import dateutil.parser
 from dateutil import tz
 import socket
-import fcntl
-import struct
+
 
 hostName = "0.0.0.0"
 serverPort = 8000
 
-import socket
+
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
@@ -21,7 +19,7 @@ def get_ip_address():
 
 
 class MyServer(BaseHTTPRequestHandler):
-    def do_POST(self):
+    def do_post(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length).decode('utf-8')
 
@@ -43,11 +41,12 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
 
-if __name__ == "__main__":  
-    logging.basicConfig(level=logging.INFO)      
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (get_ip_address(), serverPort))
-    
+
     try:
         webServer.serve_forever()
     except KeyboardInterrupt:
@@ -55,5 +54,4 @@ if __name__ == "__main__":
 
     webServer.server_close()
     print("Server stopped.")
-
     
