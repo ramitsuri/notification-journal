2e09a44eecdb2babfdb23a328c808dee3e7ced80
Ramit Suri <ramitsuri007@gmail.com>
2023-12-01 18:40:49 -0500

    Fix install script

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ WEARABLE_SERIAL="emulator-5554"
 PHONE_SERIAL="emulator-5555"
 
 ANDROID_SERIAL=$WEARABLE_SERIAL ./gradlew :wearos:installDebug
-adb -s $WEARABLE_SERIAL shell monkey -p com.ramitsuri.notificationjournal -c android.intent.category.LAUNCHER 1
+adb -s $WEARABLE_SERIAL shell monkey -p com.ramitsuri.notificationjournal.debug -c android.intent.category.LAUNCHER 1
 
 ANDROID_SERIAL=$PHONE_SERIAL ./gradlew :app:installDebug
-adb -s $PHONE_SERIAL shell monkey -p com.ramitsuri.notificationjournal -c android.intent.category.LAUNCHER 1
+adb -s $PHONE_SERIAL shell monkey -p com.ramitsuri.notificationjournal.debug -c android.intent.category.LAUNCHER 1
 adb -s $PHONE_SERIAL shell settings put system accelerometer_rotation 0
