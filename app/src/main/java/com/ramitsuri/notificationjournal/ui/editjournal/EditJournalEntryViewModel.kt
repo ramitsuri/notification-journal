d96d0cb1e75f7f9009eefef8f6027e21202192df
Ramit Suri <ramitsuri007@gmail.com>
2024-05-24 22:15:50 -0400

    Rip out load webpage title
    Since podcasts just supply the text now, loading url is no longer
    necessary

--- app/src/main/java/com/ramitsuri/notificationjournal/ui/editjournal/EditJournalEntryViewModel.kt
+++ app/src/main/java/com/ramitsuri/notificationjournal/ui/editjournal/EditJournalEntryViewModel.kt
@@ -1,6 +1,5 @@
 package com.ramitsuri.notificationjournal.ui.editjournal
 
-import android.util.Log
 import androidx.lifecycle.AbstractSavedStateViewModelFactory
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
@@ -10,11 +9,7 @@ import com.ramitsuri.notificationjournal.core.data.TagsDao
 import com.ramitsuri.notificationjournal.core.model.Tag
 import com.ramitsuri.notificationjournal.core.model.entry.JournalEntry
 import com.ramitsuri.notificationjournal.core.repository.JournalRepository
-import com.ramitsuri.notificationjournal.core.utils.loadTitle
 import com.ramitsuri.notificationjournal.di.ServiceLocator
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
@@ -24,10 +19,7 @@ class EditJournalEntryViewModel(
     savedStateHandle: SavedStateHandle,
     private val repository: JournalRepository,
     private val tagsDao: TagsDao,
-    private val loadTitle: (String, String?) -> String?,
 ) : ViewModel() {
-    private var processUrlJob: Job? = null
-
     private val _saved = MutableStateFlow(false)
     val saved: StateFlow<Boolean> = _saved
 
@@ -43,7 +35,6 @@ class EditJournalEntryViewModel(
             _state.update {
                 it.copy(isLoading = false, text = entry.text, selectedTag = entry.tag)
             }
-            loadAdditionalDataIfUrl(text = entry.text)
         }
         loadTags()
     }
@@ -52,7 +43,6 @@ class EditJournalEntryViewModel(
         _state.update {
             it.copy(text = text)
         }
-        loadAdditionalDataIfUrl(text)
     }
 
     fun tagClicked(tag: String) {
@@ -87,18 +77,6 @@ class EditJournalEntryViewModel(
         }
     }
 
-    private fun loadAdditionalDataIfUrl(text: String?) {
-        Log.d(TAG, "Canceling existing process url job")
-        processUrlJob?.cancel()
-        processUrlJob = viewModelScope.launch(Dispatchers.IO) {
-            delay(300)
-            val pageTitle = loadTitle(TAG, text)
-            _state.update {
-                it.copy(suggestedText = pageTitle)
-            }
-        }
-    }
-
     private fun loadTags() {
         viewModelScope.launch {
             _state.update {
@@ -108,7 +86,6 @@ class EditJournalEntryViewModel(
     }
 
     companion object {
-        private const val TAG = "EditJournalEntryViewModel"
         const val ENTRY_ID_ARG = "entry_id"
 
         fun factory(navBackStackEntry: NavBackStackEntry) =
@@ -126,7 +103,6 @@ class EditJournalEntryViewModel(
                         savedStateHandle = handle,
                         repository = ServiceLocator.repository,
                         tagsDao = ServiceLocator.tagsDao,
-                        loadTitle = ::loadTitle
                     ) as T
                 }
             }
