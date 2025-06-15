a865803f87bff3ac16986489f1efd33c588ba0f2
Ramit Suri <ramitsuri007@gmail.com>
2025-06-15 10:56:54 -0400

    Attempt to make Rabbit Mq connection more robust (#182)
    - Make the app use a single connection for both receive and send
    because that's what's recommended
    - Close channel after every send as they are light weight and can
    be closed
    - Return a shared flow of received payloads so can have multiple
    listeners

--- core/src/commonMain/kotlin/com/ramitsuri/notificationjournal/core/network/DataReceiveHelperImpl.kt
+++ core/src/commonMain/kotlin/com/ramitsuri/notificationjournal/core/network/DataReceiveHelperImpl.kt
@@ -1,99 +1,23 @@
 package com.ramitsuri.notificationjournal.core.network
 
-import co.touchlab.kermit.Logger
-import com.rabbitmq.client.Channel
-import com.rabbitmq.client.Connection
-import com.rabbitmq.client.ConnectionFactory
-import com.rabbitmq.client.DeliverCallback
-import com.rabbitmq.client.Delivery
+import com.ramitsuri.notificationjournal.core.di.ServiceLocator
 import com.ramitsuri.notificationjournal.core.model.DataHostProperties
-import com.ramitsuri.notificationjournal.core.model.sync.Payload
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
-import kotlinx.coroutines.withContext
-import kotlinx.serialization.json.Json
+import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.shareIn
 
 internal class DataReceiveHelperImpl(
-    private val ioDispatcher: CoroutineDispatcher,
-    private val getDataHostProperties: suspend () -> DataHostProperties,
-    private val json: Json,
+    ioDispatcher: CoroutineDispatcher,
+    getDataHostProperties: suspend () -> DataHostProperties,
+    rabbitMqHelper: RabbitMqHelper,
 ) : DataReceiveHelper {
-    private var connection: Connection? = null
-    private var channel: Channel? = null
-    private val mutex: Mutex = Mutex()
-
-    override suspend fun startListening(onPayloadReceived: (Payload) -> Unit) {
-        log("Start receiving")
-        val dataHostProperties = getDataHostProperties()
-        if (!dataHostProperties.isValid()) {
-            log("Cannot start receiving, invalid data host properties")
-            return
-        }
-        val deliverCallback =
-            DeliverCallback { _: String?, delivery: Delivery ->
-                val message = String(delivery.body, Charsets.UTF_8)
-                val payload = json.decodeFromString<Payload>(message)
-                if (payload.sender.id == dataHostProperties.deviceId) {
-                    log("Ignoring own message")
-                    return@DeliverCallback
-                }
-                onPayloadReceived(payload)
-            }
-        withContext(ioDispatcher) {
-            mutex.withLock {
-                createChannelIfNecessary(dataHostProperties)
-                try {
-                    channel?.let {
-                        it.basicConsume(dataHostProperties.deviceName, true, deliverCallback) { _ -> }
-                    } ?: log("Channel not available")
-                } catch (e: Exception) {
-                    log("Failed to setup receiver", e)
-                }
-            }
-        }
-    }
-
-    override suspend fun closeConnection() {
-        mutex.withLock {
-            runCatching {
-                connection?.close()
-                connection = null
-                channel?.close()
-                channel = null
-            }
-        }
-    }
-
-    private suspend fun createChannelIfNecessary(dataHostProperties: DataHostProperties) {
-        try {
-            if (connection == null) {
-                connection =
-                    ConnectionFactory().apply {
-                        host = dataHostProperties.dataHost
-                        username = dataHostProperties.username
-                        password = dataHostProperties.password
-                        isAutomaticRecoveryEnabled = true
-                        isTopologyRecoveryEnabled = true
-                    }.newConnection()
-                channel = connection?.createChannel()
-                channel?.queueDeclare(dataHostProperties.deviceName, true, false, false, null)
-                channel?.queueBind(dataHostProperties.deviceName, dataHostProperties.exchangeName, "")
-            }
-        } catch (e: Exception) {
-            log("Failed to connect to RabbitMQ", e)
-            closeConnection()
-        }
-    }
-
-    private fun log(
-        message: String,
-        throwable: Throwable? = null,
-    ) {
-        Logger.i(TAG, throwable) { message }
-    }
-
-    companion object {
-        private const val TAG = "DataReceiveHelper"
-    }
+    override val payloadFlow =
+        rabbitMqHelper
+            .receive(getDataHostProperties)
+            .flowOn(ioDispatcher)
+            .shareIn(
+                scope = ServiceLocator.coroutineScope,
+                started = WhileSubscribed(),
+            )
 }
--- core/src/commonMain/kotlin/com/ramitsuri/notificationjournal/core/network/DataSendHelperImpl.kt
+++ core/src/commonMain/kotlin/com/ramitsuri/notificationjournal/core/network/DataSendHelperImpl.kt
@@ -1,33 +1,17 @@
 package com.ramitsuri.notificationjournal.core.network
 
 import co.touchlab.kermit.Logger
-import com.rabbitmq.client.Channel
-import com.rabbitmq.client.Connection
-import com.rabbitmq.client.ConnectionFactory
-import com.rabbitmq.client.MessageProperties
 import com.ramitsuri.notificationjournal.core.model.DataHostProperties
 import com.ramitsuri.notificationjournal.core.model.Tag
 import com.ramitsuri.notificationjournal.core.model.entry.JournalEntry
 import com.ramitsuri.notificationjournal.core.model.sync.Payload
-import com.ramitsuri.notificationjournal.core.model.sync.Sender
 import com.ramitsuri.notificationjournal.core.model.template.JournalEntryTemplate
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
-import kotlinx.coroutines.withContext
 import kotlinx.datetime.LocalDate
-import kotlinx.serialization.json.Json
-import kotlin.time.Duration.Companion.seconds
 
 internal class DataSendHelperImpl(
-    private val ioDispatcher: CoroutineDispatcher,
     private val getDataHostProperties: suspend () -> DataHostProperties,
-    private val json: Json,
+    private val rabbitMqHelper: RabbitMqHelper,
 ) : DataSendHelper {
-    private var connection: Connection? = null
-    private var channel: Channel? = null
-    private val mutex: Mutex = Mutex()
-
     override suspend fun sendEntries(entries: List<JournalEntry>): Boolean {
         return Payload.Entries(
             data = entries,
@@ -56,81 +40,12 @@ internal class DataSendHelperImpl(
         ).send()
     }
 
-    private suspend fun Payload.send(isRetry: Boolean = false): Boolean {
+    private suspend fun Payload.send(): Boolean {
         log("Sending payload")
-        val dataHostProperties = getDataHostProperties()
-        if (!dataHostProperties.isValid()) {
-            log("Cannot send, invalid data host properties")
-            return false
-        }
-        val payloadWithSender =
-            this.attachSender(Sender(name = dataHostProperties.deviceName, id = dataHostProperties.deviceId))
-        return withContext(ioDispatcher) {
-            mutex.withLock {
-                createChannelIfNecessary(dataHostProperties)
-                try {
-                    val message = json.encodeToString(payloadWithSender).toByteArray()
-                    channel?.let {
-                        it.basicPublish(
-                            dataHostProperties.exchangeName,
-                            "",
-                            MessageProperties.PERSISTENT_TEXT_PLAIN,
-                            message,
-                        )
-                        it.waitForConfirmsOrDie(5.seconds.inWholeMilliseconds)
-                        true
-                    } ?: run {
-                        log("Channel not available")
-                        false
-                    }
-                } catch (e: Exception) {
-                    log("failed to send message", e)
-                    if (isRetry) {
-                        return@withContext false
-                    } else {
-                        closeConnectionInternal()
-                        return@withContext send(isRetry = true)
-                    }
-                }
-            }
-        }
-    }
-
-    override suspend fun closeConnection() {
-        log("Closing connection")
-        mutex.withLock {
-            closeConnectionInternal()
-        }
-    }
-
-    private fun closeConnectionInternal() {
-        runCatching {
-            connection?.close()
-            connection = null
-            channel?.close()
-            channel = null
-            log("Connection closed")
-        }
-    }
-
-    private fun createChannelIfNecessary(dataHostProperties: DataHostProperties) {
-        try {
-            if (connection == null) {
-                connection =
-                    ConnectionFactory().apply {
-                        host = dataHostProperties.dataHost
-                        username = dataHostProperties.username
-                        password = dataHostProperties.password
-                        isAutomaticRecoveryEnabled = true
-                        isTopologyRecoveryEnabled = true
-                    }.newConnection()
-                channel = connection?.createChannel()
-                channel?.confirmSelect()
-            }
-        } catch (e: Exception) {
-            log("Failed to connect to RabbitMQ", e)
-            closeConnectionInternal()
-        }
+        return rabbitMqHelper.send(
+            getDataHostProperties = getDataHostProperties,
+            payload = this,
+        )
     }
 
     private fun log(
