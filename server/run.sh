2b109e72245c17231756478d7547d88b34bdc2ce
Ramit Suri <ramitsuri007@gmail.com>
2025-07-03 20:43:53 -0400

    Remove old server (#199)

--- server/run.sh
+++
@@ -1,9 +0,0 @@
-# cron with
-# @reboot path/to/run.sh
-tmux has-session -t 'journal-server' 2>/dev/null
-if [ $? != 0 ]; then
-  tmux new-session -d -s 'journal-server' 'python main.py'
-else
-  tmux kill-session -t journal-server
-  tmux new-session -d -s 'journal-server' 'python main.py'
-fi
