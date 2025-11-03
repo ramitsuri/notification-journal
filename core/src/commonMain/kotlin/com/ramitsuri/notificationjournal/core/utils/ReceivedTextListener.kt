9654f9612815ed48cfb83b833d10ec1eb3d9b760
Ramit Suri <ramitsuri007@gmail.com>
2025-11-02 20:05:34 -0500

    Allow receiving intent (#226)

--- core/src/commonMain/kotlin/com/ramitsuri/notificationjournal/core/utils/ReceivedTextListener.kt
+++ core/src/commonMain/kotlin/com/ramitsuri/notificationjournal/core/utils/ReceivedTextListener.kt
@@ -2,9 +2,22 @@ package com.ramitsuri.notificationjournal.core.utils
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
 
 @Composable
 expect fun ReceivedTextListener(
     navController: NavController,
-    onTextReceived: (String?) -> Unit,
+    onTextReceived: (ReceivedTextProperties?) -> Unit,
 )
+
+data class ReceivedTextProperties(
+    val text: String?,
+    val tag: String? = null,
+)
+
+@OptIn(ExperimentalContracts::class)
+fun ReceivedTextProperties?.hasValues(): Boolean {
+    contract { returns(true) implies (this@hasValues != null) }
+    return this != null && !text.isNullOrEmpty()
+}
